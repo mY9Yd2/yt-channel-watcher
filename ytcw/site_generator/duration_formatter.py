d0fc0c72c9978b718119ec1d35481aef8037b742
Kovács József Miklós <kovacsjozsef7u@gmail.com>
2024-08-03 09:48:26 +0200

    feat: add support for days in the duration_formatter

--- ytcw/site_generator/duration_formatter.py
+++ ytcw/site_generator/duration_formatter.py
@@ -10,6 +10,7 @@ def duration_to_machine_readable(duration: str) -> str:
     For example 4 hours 18 minutes and 3 seconds (4:18:03): PT4H18M3S
 
     See: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/time#a_valid_duration_string
+    or https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#durations
 
     Args:
         duration (str): Duration string
@@ -23,19 +24,30 @@ def duration_to_machine_readable(duration: str) -> str:
 
     parts = list(map(int, duration.split(":")))
 
-    if len(parts) == 3:
+    if len(parts) == 4:
+        days, hours, minutes, seconds = parts
+    elif len(parts) == 3:
+        days = 0
         hours, minutes, seconds = parts
     elif len(parts) == 2:
+        days = 0
         hours = 0
         minutes, seconds = parts
     elif len(parts) == 1:
+        days = 0
         hours = 0
         minutes = 0
         seconds = parts[0]
     else:
         raise ValueError("Invalid duration format")
 
-    label_parts = ["PT"]
+    label_parts = ["P"]
+
+    if days > 0:
+        label_parts.append(f"{days}D")
+
+    label_parts.append("T")
+
     if hours > 0:
         label_parts.append(f"{hours}H")
     if minutes > 0:
